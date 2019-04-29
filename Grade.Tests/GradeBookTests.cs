81c89509e2359ab5908dc7a0af62ceefeb08bd30
Stephen McKeown <stephen.mckeown@sigma.se>
2019-04-29 11:18:48 +0100

    Array test code

--- Grade.Tests/GradeBookTests.cs
+++ Grade.Tests/GradeBookTests.cs
@@ -13,6 +13,22 @@ namespace Grades.Tests
     public class GradeBookTests
     {
         [TestMethod]
+        public void UsingArrays()
+        {
+            float[] grades;
+            grades = new float[3];
+
+            AddGrades(grades);
+
+            Assert.AreEqual(89.1f, grades[1]);
+        }
+
+        private void AddGrades(float[] grades)
+        {
+            grades[1] = 89.1f;
+        }
+
+        [TestMethod]
         public void ComputesHigestGrade()
         {
             GradeBook book = new GradeBook();
